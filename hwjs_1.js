80e5597462a21de3a05483fea90b6cc58b26ae3c
sonybob <kutsmg@gmail.com>
2022-03-18 15:05:34 +0200

    Homework JS #1_finish

--- hwjs_1.js
+++ hwjs_1.js
@@ -1,1 +1,18 @@
-"use strici";
+"use strict";
+let numberOfFilms = +prompt("How match films look", 0);
+const a = prompt("What last film", ""),
+      b = +prompt("What mark film", 0),
+      c = prompt("What last film", ""),
+      d = +prompt("What mark film", 0);
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
